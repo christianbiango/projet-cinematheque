754db14d5f2b39147640aa004e8fd4e8fcb5b970
Melix1 <biangojunior@gmail.com>
2024-03-30 00:07:13 +0100

    Création du controlleur d'intégration Mongo des films

---
+++ backend/controllers/movie.controller.js
@@ -0,0 +1,41 @@
+import movieSchema from "../models/movie.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { env } from "../config/index.js";
+import Movie from "../models/movie.model.js";
+
+// Utils
+import {
+  retrieveExcelMovies,
+  formatExcelMovies,
+  countDBMovies,
+  createMoviesBulkUpdateArray,
+} from "../utils/movie.helper.js";
+
+/**
+ * Cette fonction  permet de récupérer la liste des films du fichier excel et les enregistrer dans la base de données
+ */
+export const saveMoviesToDB = async () => {
+  try {
+    const movies = retrieveExcelMovies();
+    console.log("Les films ont été récupérés du fichier Excel");
+
+    const formattedMovies = formatExcelMovies(movies);
+    console.log("Les films ont été mis au bon format Mongo");
+
+    // Sauvegarder tous les films si la bdd est vide, sinon mettre à jour
+    const moviesCount = await countDBMovies();
+
+    if (moviesCount > 0) {
+      const bulkData = await createMoviesBulkUpdateArray(formattedMovies);
+      await Movie.bulkWrite(bulkData);
+      console.log("Mise à jour réussie de tous les films.");
+    } else {
+      // Bdd vide
+      await Movie.insertMany(formattedMovies);
+      console.log("Importation des films avec succès dans la base de données.");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
